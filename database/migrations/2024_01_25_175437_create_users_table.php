49976efcf4e0b6d913fad6d1a22c950d40d0d924
Quinchy <deguzmancyriljames@gmail.com>
2024-01-25 18:09:43 +0800

    .

--- database/migrations/2024_01_25_175437_create_users_table.php
+++ database/migrations/2024_01_25_175437_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('google_id')->nullable();
             $table->string('google_token')->nullable();
             $table->rememberToken();
